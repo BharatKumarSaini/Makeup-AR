8b2af33d1546b8a8ba1a28665c3a7312b6f8a256
BharatKumarSaini <bharatskataria@gmail.com>
2022-11-07 18:25:28 +0530

    initial commit

---
+++ Assets/Scripts/MakeupSwap.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class MakeupSwap : MonoBehaviour
+{
+    private ARFaceManager faceManager;
+    public List<Material> faceMaterials = new List<Material>(); 
+    private int faceMaterialIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        faceManager = GetComponent<ARFaceManager>();
+    }
+
+    public void SwitchMakeup()
+    {   
+        foreach (ARFace face in faceManager.trackables)
+        {
+            face.GetComponent<Renderer>().material = faceMaterials[faceMaterialIndex];
+                   
+        }
+
+       faceMaterialIndex ++; 
+        if(faceMaterialIndex == faceMaterials.Count)
+        {
+            faceMaterialIndex = 0;
+        }
+    }
+}
+ 
